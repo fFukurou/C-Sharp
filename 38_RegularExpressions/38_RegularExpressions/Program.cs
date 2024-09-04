0ab51ec119ee0440272c1d9b4cfa238911b7215f
fFukurou <supevi1@gmail.com>
2024-09-04 19:49:46 -0300

    Regex challenges, WPF start

--- 38_RegularExpressions/38_RegularExpressions/Program.cs
+++ 38_RegularExpressions/38_RegularExpressions/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace _38_RegularExpressions
+﻿using System.Text.RegularExpressions;
+
+namespace _38_RegularExpressions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string pattern = @"\d{5}";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi there my number is 12345";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine($"{matchCollection.Count} hits found:\n {text}");
+
+            foreach (Match hit in matchCollection) {
+
+                GroupCollection group = hit.Groups;
+                Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
+            
+            }
+
+            // EMAIL EXTRACTOR
+
+            string emailPattern = @"\w+@\w+\.\w+";
+            regex = new Regex(emailPattern);
+
+            string email = "Contact us at support@example.com or sales@example.org";
+
+            matchCollection = regex.Matches(email);
+
+            Console.WriteLine($"\n\n{matchCollection.Count} hits (emails) found:\n {email}");
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
+
+            }
+
+
+
+
+
         }
     }
 }
---
+++ 40_WPF/40_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _40_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
